e970d8b75cbcf7285072134870e64d826b6f5fc2
Lee ByungSeo <bslee@yahoo.com>
2026-06-12 21:06:22 +0900

    Update run_all_agents.py

--- run_all_agents.py
+++ run_all_agents.py
@@ -1,9 +1,14 @@
 from agents.nrc_agent import main as run_nrc
+from agents.ap1000_agent import main as run_ap1000
+from agents.apr1400_agent import main as run_apr1400
 
 def main():
-    print("Starting GNDO Data Factory")
+
+    print("Starting GNDO")
 
     run_nrc()
+    run_ap1000()
+    run_apr1400()
 
     print("Finished")
 
