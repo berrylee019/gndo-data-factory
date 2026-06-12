15c7cad4381b54ab6281ee3e9037f3e889d197ee
Lee ByungSeo <bslee@yahoo.com>
2026-06-12 20:09:32 +0900

    Create run_all_agents.py

---
+++ run_all_agents.py
@@ -0,0 +1,7 @@
+from agents.nrc_agent import run_nrc_agent
+from agents.ap1000_agent import run_ap1000_agent
+from agents.apr1400_agent import run_apr1400_agent
+
+run_nrc_agent()
+run_ap1000_agent()
+run_apr1400_agent()
