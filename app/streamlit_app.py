fd64f5b7320aa68fee876164b028ac8b3bc264eb
Lee ByungSeo <bslee@yahoo.com>
2026-06-20 22:05:35 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -209,9 +209,7 @@ with tab3:
         st.subheader(
             "GNDO Search"
         )
-        st.write(
-            rkg_df.columns.tolist()
-        )
+
         gndo_search = st.text_input(
             "Search GNDO Objects"
         )
