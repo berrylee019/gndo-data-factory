2ee800f273bb65303a39f8dc59605bfcfabe569e
Lee ByungSeo <bslee@yahoo.com>
2026-06-22 19:19:43 +0900

    Update rkg_agent_v12.py

--- agents/rkg_agent_v12.py
+++ agents/rkg_agent_v12.py
@@ -1,1 +1,11 @@
+RKG_V11_FILE = (
+    "storage/metadata/rkg_data_v11.json"
+)
 
+CHANGE_FILE = (
+    "storage/rkg/change_registry_v12.json"
+)
+
+OUTPUT_FILE = (
+    "storage/metadata/rkg_data_v12.json"
+)
