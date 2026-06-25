85d1b45f8f312b8dea9b054eb6b2074284369c2e
Lee ByungSeo <bslee@yahoo.com>
2026-06-25 21:18:57 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -435,7 +435,6 @@ for _, r in rkg_df.iterrows():
         )
         
         ask_gndo = st.text_input(
-        st.write("HERE-438"),
             "Ask a Traceability Question",
             placeholder="예: FAIL-CH07-001의 영향은?",
             key="ask_gndo_input_v14"
