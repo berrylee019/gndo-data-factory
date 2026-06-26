ebfeea6e8b9d20cac45f23140281c3d57f8c4016
Lee ByungSeo <bslee@yahoo.com>
2026-06-26 19:56:52 +0900

    Update graph_builder.py

--- engine/graph_builder.py
+++ engine/graph_builder.py
@@ -1,1 +1,38 @@
+import networkx as nx
+import pandas as pd
 
+
+class GraphBuilder:
+
+    @staticmethod
+    def build(rkg_df):
+
+        G = nx.DiGraph()
+
+        for _, r in rkg_df.iterrows():
+
+            if pd.notna(r.get("change_id")) and pd.notna(r.get("affected_requirement")):
+                G.add_edge(
+                    r["change_id"],
+                    r["affected_requirement"]
+                )
+
+            if pd.notna(r.get("affected_requirement")) and pd.notna(r.get("affected_verification")):
+                G.add_edge(
+                    r["affected_requirement"],
+                    r["affected_verification"]
+                )
+
+            if pd.notna(r.get("affected_verification")) and pd.notna(r.get("affected_test")):
+                G.add_edge(
+                    r["affected_verification"],
+                    r["affected_test"]
+                )
+
+            if pd.notna(r.get("affected_test")) and pd.notna(r.get("failure_id")):
+                G.add_edge(
+                    r["affected_test"],
+                    r["failure_id"]
+                )
+
+        return G
