f54a3fd8011431b3a56e1d9d7a1ee13cc0d7b2c4
Lee ByungSeo <bslee@yahoo.com>
2026-06-26 19:58:07 +0900

    Update propagation_engine.py

--- engine/propagation_engine.py
+++ engine/propagation_engine.py
@@ -1,1 +1,13 @@
+from engine.graph_builder import GraphBuilder
+from engine.semantic_expansion import SemanticExpansion
 
+import networkx as nx
+
+
+class ImpactPropagationEngine:
+
+    def __init__(self, rkg_df):
+
+        self.rkg_df = rkg_df
+
+        self.G = GraphBuilder.build(rkg_df)
