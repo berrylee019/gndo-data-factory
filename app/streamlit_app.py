ede950710eb5b3c26d24161a924e59613c295f8c
Lee ByungSeo <bslee@yahoo.com>
2026-06-26 21:05:02 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,12 @@
+import os
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+    
 import streamlit as st
 import json
 import pandas as pd
