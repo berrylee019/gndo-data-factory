af7dcac4a82f84b8d86c81dd165dccec79ed34d9
Lee ByungSeo <bslee@yahoo.com>
2026-06-26 21:13:23 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -14,8 +14,8 @@ import streamlit as st
 import json
 import pandas as pd
 from pathlib import Path
+from engine.graph_builder import GraphBuilder
 
-from engine.propagation_engine import PropagationEngine
 import networkx as nx
 
 from pyvis.network import Network
