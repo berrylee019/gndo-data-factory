b99da941dfd06598142b173142e389a1bd8f3541
Lee ByungSeo <bslee@yahoo.com>
2026-06-27 14:03:09 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -252,14 +252,6 @@ with tab3:
     st.write(rkg_df.shape)
     st.write(rkg_df.columns.tolist())
 
-    print(rkg_df.iloc[0][[
-        "change_id",
-        "affected_requirement",
-        "affected_verification",
-        "affected_test",
-        "failure_id"
-    ]])
-
     GRAPH = GraphBuilder.build(rkg_df)
     
     st.success("✅ GraphBuilder Loaded")
