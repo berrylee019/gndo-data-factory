039e44c364421f52e7a0b47b1b13333353006293
Lee ByungSeo <bslee@yahoo.com>
2026-07-06 20:09:39 +0900

    Refactor GraphVisualizer and update node styles
    Refactor GraphVisualizer to use a new node style structure and simplify the build_network method.

--- gndo_engine/graph_visualizer.py
+++ gndo_engine/graph_visualizer.py
@@ -1,70 +1,99 @@
 from pyvis.network import Network
 import tempfile
 
+NODE_STYLE = {
+
+    "CHANGE": {
+        "color": "#ff3b30",
+        "size": 36,
+        "shape": "dot"
+    },
+
+    "REQ": {
+        "color": "#34c759",
+        "size": 32,
+        "shape": "dot"
+    },
+
+    "VER": {
+        "color": "#ffcc00",
+        "size": 30,
+        "shape": "dot"
+    },
+
+    "TEST": {
+        "color": "#007aff",
+        "size": 30,
+        "shape": "dot"
+    },
+
+    "FAIL": {
+        "color": "#8e24aa",
+        "size": 32,
+        "shape": "dot"
+    },
+
+    "SYSTEM": {
+        "color": "#26c6da",
+        "size": 26,
+        "shape": "box"
+    },
+
+    "ARTIFACT": {
+        "color": "#9e9e9e",
+        "size": 24,
+        "shape": "box"
+    }
+
+}
+
 
 class GraphVisualizer:
 
     @staticmethod
-    def build_network(
-        G,
-        height="800px",
-        width="100%"
-    ):
+    def build_network(G):
 
         net = Network(
-            height=height,
-            width=width,
+            height="650px",
+            width="100%",
+            directed=True,
             bgcolor="#ffffff",
-            font_color="black",
-            directed=True
+            font_color="#222222"
         )
 
-        net.from_nx(G)
+        for node, attr in G.nodes(data=True):
 
-        ##################################################
-        # Node Color
-        ##################################################
+            style = NODE_STYLE.get(
+                attr.get("type"),
+                {
+                    "color":"#cccccc",
+                    "size":25,
+                    "shape":"dot"
+                }
+            )
 
-        color_map = {
-            "CHANGE": "#ff0000",
-            "REQ": "#00cc66",
-            "VER": "#0099ff",
-            "TEST": "#ffaa00",
-            "FAIL": "#9900cc"
-        }
+            net.add_node(
+                node,
+                label=node,
+                color=style["color"],
+                size=style["size"],
+                shape=style["shape"]
+            )
 
-        for node in net.nodes:
+        for u, v, attr in G.edges(data=True):
 
-            node_type = node.get("type")
+            net.add_edge(
+                u,
+                v,
+                arrows="to",
+                color="#666666",
+                width=2
+            )
 
-            if node_type in color_map:
-                node["color"] = color_map[node_type]
-
-            node["shape"] = "dot"
-            node["size"] = 18
-
-        ##################################################
-        # Edge Style
-        ##################################################
-
-        for edge in net.edges:
-
-            edge["width"] = 3
-
-            edge["arrows"] = "to"
-
-            edge["smooth"] = {
-                "type": "dynamic"
-            }
-
-            edge["font"] = {
-                "size": 14,
-                "align": "middle"
-            }
+        net.toggle_physics(True)
 
         return net
 
-    ##################################################
 
     @staticmethod
     def save_html(net):
@@ -76,11 +105,7 @@ class GraphVisualizer:
 
             net.save_graph(tmp.name)
 
-            with open(
+            return open(
                 tmp.name,
                 encoding="utf-8"
-            ) as f:
-
-                html = f.read()
-
-        return html
+            ).read()
